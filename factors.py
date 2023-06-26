07e5d1da75304f8f9dcb6fc6a2fab56b9698d9a0
KSI5 <krissigebs@gmail.com>
2023-06-26 11:51:55 -0700

    updated the factor file

--- factors.py
+++ factors.py
@@ -2,56 +2,53 @@
 
 import sys
 import time
+import math
 
-
-def factorize(num):
+def factorize(number):
     '''Takes a number as input.
     Args:
-        num: input integer.
-    Return: A list of tuples, each containing a factorization pair (p, q).
+        number: input integer.
+    Returns: A list of tuples, each containing a factorization pair (p, q).
     '''
     factors = []
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            factors.append((i, num // i))
+    limit = math.isqrt(number) + 1
+    for i in range(2, limit):
+        if number % i == 0:
+            factors.append((i, number // i))
     return factors
 
 
 if __name__ == "__main__":
-    '''Reads the input file.
-    '''
+    '''Reads the input file.'''
     if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-        exit()
+        raise ValueError("Usage: factors <file>")
 
     input_file = sys.argv[1]
 
     try:
         with open(input_file, 'r') as f:
-            lines = f.readlines()
+            numbers = f.readlines()
     except FileNotFoundError:
-        print("File not found")
-        exit()
+        raise FileNotFoundError("File not found")
 
     start_time = time.time()
 
-    '''loops over each line (which should contain one natural number per line),
-        and calls factorize on each number.
-        If factorize returns a list of factors,
-        it prints the factorization in the desired format.
+    '''Loops over each line (which should contain one natural number per line),
+    and calls factorize on each number.
+    If factorize returns a list of factors,
+    it prints the factorization in the desired format.
     '''
-    for line in lines:
-        num = int(line.strip())
-        factors = factorize(num)
+    for line in numbers:
+        number = int(line.strip())
+        factors = factorize(number)
         if factors:
             for factor in factors:
                 p, q = factor
-                print(f"{num}={p}*{q}")
+                print(f"{number}={p}*{q}")
 
         '''If the elapsed time exceeds 5 seconds,
-            the program exits with an error message.
+        the program exits with an error message.
         '''
         if time.time() - start_time > 5:
-            print("Time limit exceeded")
-            exit()
+            raise RuntimeError("Time limit exceeded")
 
